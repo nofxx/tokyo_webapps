477d300cde8c6e522a8449a1c5664486bd687c6b
Marcos Piccinini <x@nofxx.com>
2009-07-03 05:35:55 -0300

    cleanup

--- rails/session/config/environment.rb
+++ rails/session/config/environment.rb
@@ -1,14 +1,6 @@
 RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
-#require "dm-tokyo-adapter"
-#tyrant = { :adapter => 'tokyo_tyrant', :host => 'localhost', :port => '1978' }
-#cabinet = { :adapter  => 'tokyo_cabinet', :database => 'dmaria', :path => "/tmp/" } #File.dirname(__FILE__) }
-#pg = { :adapter => 'postgres', :database => 'dmaria_test', :user => 'postgres'}
-#adapter = ENV["TYRANT"] ? tyrant : cabinet
-#DataMapper.setup(:default,tyrant) #tyrant) # adapter)
-#DataMapper.logger = Logger.new($stderr)#Rails.logger
-
 Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -17,7 +9,8 @@ Rails::Initializer.run do |config|
   #config.gem 'dm-core'
   # config.gem 'dm-aggregates'
   # config.gem 'do_sqlite3'
-  config.gem 'tokyo_session_store'
+  config.gem 'rufus-tokyo'
+  config.gem 'tokyo_store'
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
   config.time_zone = 'UTC'
 
--- rails/session/config/initializers/datamapper.rb
+++ rails/session/config/initializers/datamapper.rb
@@ -1,8 +1,8 @@
- require "dm-core"
-require "dm-aggregates"
-# # require "dm-timestamps"
-# #require "dm-migrations"
-require "dm-validations"
+require "dm-core"
+# require "dm-aggregates"
+# require "dm-timestamps"
+# require "dm-migrations"
+# require "dm-validations"
 
 DataMapper::Logger.new(STDOUT, :debug) # :off, :fatal, :error, :warn, :info, :debug
 hash = YAML.load(File.new(RAILS_ROOT + "/config/database.yml"))
